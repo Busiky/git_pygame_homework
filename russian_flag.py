42b20c277ec113be9d58cf9142816ccb0195ce67
Busiky <ya.busiky@gmail.com>
2019-08-05 20:43:49 +0300

    Заготовка файла pygame

---
+++ russian_flag.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+
+def draw():
+    pass
+
+
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
