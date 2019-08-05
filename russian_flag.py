a46006e1d711c0fdfdfd7213a52a72b8947963c3
Busiky <ya.busiky@gmail.com>
2019-08-05 21:03:24 +0300

    Белая полоса

--- russian_flag.py
+++ russian_flag.py
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode(size)
 
 def draw():
     pygame.draw.rect(screen, pygame.Color('#65350f'), (20, 20, 30, 460))
+    pygame.draw.rect(screen, pygame.Color('white'), (50, 220, 430, 100))
 
 
 draw()
